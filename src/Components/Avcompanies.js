fe66b65807fbc1edf1feea359395d40e9729f59e
vrajesh-art <vrajeshshetty2000@gmail.com>
2023-09-21 08:36:42 +0530

    first commit

---
+++ src/Components/Avcompanies.js
@@ -0,0 +1,115 @@
+import React from 'react'
+import './avcompanies.css'
+import godrej from './images/logo-godrej.png'
+import oppo from './images/logo-oppo.png'
+import cococola from './images/logo-coca-cola.png'
+import philips from './images/logo-philips.png'
+import paypal from './images/logo-paypal.png'
+const Avcompanies = () => {
+    return (
+        <>
+
+            <div className="offer" id="about">
+                <div className="small-container">
+                    <h2 className="title">Top Companies At Our Website</h2>
+                    <div className="brands">
+                        <div className="small-container">
+                            <div className="row">
+                                <div className="col-5">
+                                    <img src={godrej} alt="Godrej" />
+                                </div>
+
+                                <div className="col-5">
+                                    <img src={oppo} alt="Oppo" />
+                                </div>
+
+                                <div className="col-5">
+                                    <img src={cococola} alt="coca-cola" />
+                                </div>
+
+                                <div className="col-5">
+                                    <img src={paypal} alt="paypal" />
+                                </div>
+
+                                <div className="col-5">
+                                    <img src={philips} alt="philips" />
+                                </div>
+
+                                <div className="col-5">
+                                    <img src={godrej} alt="Godrej" />
+                                </div>
+
+                                <div className="col-5">
+                                    <img src={oppo} alt="Oppo" />
+                                </div>
+
+                                <div className="col-5">
+                                    <img src={cococola} alt="coca-cola" />
+                                </div>
+
+                                <div className="col-5">
+                                    <img src={paypal} alt="paypal" />
+                                </div>
+
+                                <div className="col-5">
+                                    <img src={philips} alt="philips" />
+                                </div>
+                                <div className="col-5">
+                                    <img src={godrej} alt="Godrej" />
+                                </div>
+
+                                <div className="col-5">
+                                    <img src={oppo} alt="Oppo" />
+                                </div>
+
+                                <div className="col-5">
+                                    <img src={cococola} alt="coca-cola" />
+                                </div>
+
+                                <div className="col-5">
+                                    <img src={paypal} alt="paypal" />
+                                </div>
+
+                                <div className="col-5">
+                                    <img src={philips} alt="philips" />
+                                </div>
+
+                                <div className="col-5">
+                                    <img src={godrej} alt="Godrej" />
+                                </div>
+
+                                <div className="col-5">
+                                    <img src={oppo} alt="Oppo" />
+                                </div>
+
+                                <div className="col-5">
+                                    <img src={cococola} alt="coca-cola" />
+                                </div>
+
+                                <div className="col-5">
+                                    <img src={paypal} alt="paypal" />
+                                </div>
+
+                                <div className="col-5">
+                                    <img src={philips} alt="philips" />
+                                </div>
+                                <div className="col-5">
+                                    <img src={paypal} alt="paypal" />
+                                </div>
+
+                                <div className="col-5">
+                                    <img src={philips} alt="philips" />
+                                </div>
+
+                                <h2 className="brandText">100+ Companies & More</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+export default Avcompanies
